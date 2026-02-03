f532d087e363c4b3abfba9d3858082b484e99037
Samrudhi-12 <168064162+Samrudhi-12@users.noreply.github.com>
2026-02-03 13:39:17 +0530

    Add files via upload

---
+++ learn.py
@@ -0,0 +1,37 @@
+age = 20
+print ( age )
+
+x = 5
+x = 25
+print (x)
+
+name = 'samrudhi'
+marks = 23
+print (name , marks)
+
+num = 6
+print ( num * 5 )
+
+count = 1
+count = 2
+print (count)
+
+name = 'sam'
+print ("Hello" , name )
+print ( 5*name )
+print(" Python "  *  5 )
+print (' * ' * 7 )
+
+word = "Learning"
+print (word [0:3])
+
+print ( word[4:8])
+print(word[3:5])
+
+name = 'Samrudhi'
+print("Hello" ,name)
+
+name = "Sam"
+age = 20
+print ("My name is " , name ,"& my age is ", age )
+
